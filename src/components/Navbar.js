f32721c45cd3d16c9418b9e016478310ebb6d184
Daler <alyamov.daler.secure@gmail.com>
2021-01-15 04:00:56 +0500

    Добавлен счетчик

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
 
 const Navbar = () => {
+  let todos = useSelector(state => state.axiosTodos)
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <Link to='/' className='navbar-brand'>Alif test</Link>
+        <h2>{todos.length && todos.length}</h2>
       </div>
     </nav>
   )
